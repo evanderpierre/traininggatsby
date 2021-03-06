7004ab5f35c14fa1ee2e6246ea9c7d58113b5d63
evanderpierre <epierre68@gmail.com>
2021-03-06 16:25:21 -0500

    added about page

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import { Link } from 'gatsby'
+
+
+const IndexPage = () => {
+  return (
+
+<Link to='/about'>
+  Go to the About page
+</Link>
+  )
+}
+
+export default IndexPage
